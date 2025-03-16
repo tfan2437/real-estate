9cfdcfd41ba1006586e8f057f48bc55c558f8ac4
Ting Wei Fan <tfan2437@gmail.com>
2025-03-16 17:45:00 +0800

    feat: add Font Awesome icons and update landing page with footer section
    - Added Font Awesome dependencies: @fortawesome/fontawesome-svg-core, @fortawesome/free-brands-svg-icons, and @fortawesome/react-fontawesome.
    - Updated landing page to include FooterSection component for improved layout.

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/app/(nondashboard)/landing/HeroSection";
 import FeatureSection from "@/app/(nondashboard)/landing/FeatureSection";
 import DiscoverSection from "@/app/(nondashboard)/landing/DiscoverSection";
 import CallToAction from "@/app/(nondashboard)/landing/CallToAction";
+import FooterSection from "@/app/(nondashboard)/landing/FooterSection";
 
 const Landing = () => {
   return (
@@ -11,6 +12,7 @@ const Landing = () => {
       <FeatureSection />
       <DiscoverSection />
       <CallToAction />
+      <FooterSection />
     </div>
   );
 };
